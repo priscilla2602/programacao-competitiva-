bdbf6e75e30c9e1a65c2e683e1b227bf10af6bcc
Priscilla Louzada Nesio <81877690+priscilla2602@users.noreply.github.com>
2026-06-08 19:05:37 -0300

    atividade5.py

--- PROGRAMAÇÃO COMPETITIVA I/QUESTOES/atividade5.py
+++ PROGRAMAÇÃO COMPETITIVA I/QUESTOES/atividade5.py
@@ -1,1 +1,24 @@
 #RESTAURANDO 3 NUMEROS
+
+import sys
+
+def main():
+    dados_entrada = sys.stdin.read().split()
+    if not dados_entrada:
+        return
+    
+    numeros = [int(x) for x in dados_entrada]
+    numeros.sort()
+    
+    soma_total = numeros[3]
+    val1 = numeros[0]  
+    val2 = numeros[1]  
+    val3 = numeros[2]  
+    
+    a = soma_total - val3
+    b = soma_total - val2
+    c = soma_total - val1
+    print(f"{a} {b} {c}")
+
+if __name__ == '__main__':
+    main()
